d256e517aa1db7b34819c6da98c5a630706d0377
Joyce Liu <liujoycec@gmail.com>
2015-11-08 20:21:04 -0800

    Changed CORS header to allow all domains

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = exports.io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
